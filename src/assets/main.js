d02272ba658c6de4f3d540449e26bbcb8acc8f85
Theo Deyle II <theod@bunsen.DEYLE>
2017-09-28 10:10:10 -0400

    Finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/978839.json',
+  	dataType: 'jsonp',
+  	success: function(response) {
+  		console.log(response);
+	  	$.each(response.courses.completed, function(index, course) {
+	  		console.log(course);
+		  	$('<div class="course"><h3 title="course.title">' + course.title + '</h3><img src=' + course.badge + '></img><a href=' + course.url + ' target="_blank" class="btn btn-primary">See Course</div>').appendTo('#badges');  		});
+  	}
 
+  });
 });
